b9b34dd191f2e401c3d94f4f0eebdec39f8828bc
Kira818 <731064925@qq.com>
2020-07-05 21:59:36 +0800

    the solution of question 2

--- 2.两数相加/2.两数相加.go
+++ 2.两数相加/2.两数相加.go
@@ -11,5 +11,35 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	l3 := &ListNode{}
+	p := l3
+	carry := 0
+	var l1Val, l2Val int
 
+	for l1 != nil || l2 != nil || carry != 0 {
+		l1Val = 0
+		if l1 != nil {
+			l1Val = l1.Val
+			l1 = l1.Next
+		}
+
+		l2Val = 0
+		if l2 != nil {
+			l2Val = l2.Val
+			l2 = l2.Next
+		}
+
+		p.Val = l1Val + l2Val + carry
+		carry = p.Val / 10
+		p.Val = p.Val % 10
+		if l1 != nil || l2 != nil || carry != 0 {
+			p.Next = &ListNode{}
+			p = p.Next
+		} else {
+			p.Next = nil
+			break
+		}
+	}
+
+	return l3
 }
