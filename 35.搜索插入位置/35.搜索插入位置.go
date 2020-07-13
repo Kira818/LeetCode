8c47878e91ab774066dec49837fcdddfe4f3d379
Kira818 <731064925@qq.com>
2020-07-13 22:08:31 +0800

    the solution of question 1

--- 35.搜索插入位置/35.搜索插入位置.go
+++ 35.搜索插入位置/35.搜索插入位置.go
@@ -1,7 +1,36 @@
 // @Author:  limuye@sgepri.sgcc.com.cn
-// @Desc:    
+// @Desc:    二分查找
 // @Version: V1.0.0
 // @Create:  2020/7/5 22:07
 
-package _5_搜索插入位置
+package LeetCode
 
+func searchInsert(nums []int, target int) int {
+	l := 0
+	r := len(nums) - 1
+	var m int
+
+	for l <= r {
+		m = (l + r) / 2
+
+		if nums[m] == target {
+			return m
+		}
+
+		if nums[m] < target {
+			l = m + 1
+		} else {
+			r = m - 1
+		}
+	}
+
+	if l == 0 {
+		return 0
+	}
+
+	if r == len(nums) {
+		return len(nums)
+	}
+
+	return r - 1
+}
