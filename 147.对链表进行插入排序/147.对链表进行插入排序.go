3bd0b5a190f9fd8d9be448e545a00a39638e0388
Kira818 <731064925@qq.com>
2020-07-20 20:37:55 +0800

    the solution of question 147

---
+++ 147.对链表进行插入排序/147.对链表进行插入排序.go
@@ -0,0 +1,15 @@
+// @Author:  limuye@sgepri.sgcc.com.cn
+// @Desc:    排序 链表
+// @Version: V1.0.0
+// @Create:  2020/7/20 18:51
+
+package LeetCode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func insertionSortList(head *ListNode) *ListNode {
+
+}
