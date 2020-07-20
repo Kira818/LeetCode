9b3c437b42cba790bdacadb85525406d2629b42e
Kira818 <731064925@qq.com>
2020-07-20 18:26:57 +0800

    the solution of question 20

--- 20.有效的括号/20.有效的括号.go
+++ 20.有效的括号/20.有效的括号.go
@@ -1,7 +1,31 @@
 // @Author:  limuye@sgepri.sgcc.com.cn
-// @Desc:    
+// @Desc:    栈 字符串
 // @Version: V1.0.0
 // @Create:  2020/7/17 22:47
 
-package _0_有效的括号
+package LeetCode
 
+var cMap = map[byte]byte{')': '(', ']': '[', '}': '{'}
+
+func isValid(s string) bool {
+	stack := make([]byte, 0)
+	b := []byte(s)
+
+	for _, c := range b {
+		if c == '(' || c == '[' || c == '{' {
+			stack = append(stack, c)
+		} else if len(stack) == 0 {
+			return false
+		} else if cMap[c] == stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
+		}
+	}
+
+	if len(stack) != 0 {
+		return false
+	}
+
+	return true
+}
