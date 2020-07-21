22ca1775b02b19588c4fcdd37185e283e6b5e23d
Kira818 <731064925@qq.com>
2020-07-21 23:14:47 +0800

    the solution of question 147

--- 0147对链表进行插入排序/0147对链表进行插入排序.go
+++ 0147对链表进行插入排序/0147对链表进行插入排序.go
@@ -11,5 +11,34 @@ type ListNode struct {
 }
 
 func insertionSortList(head *ListNode) *ListNode {
+	res := &ListNode{0, nil}
+	pre := res
+	var tmpPre *ListNode
 
+	for cur := head; cur != nil; cur = cur.Next {
+		if pre.Next == nil {
+			pre.Next = head
+			pre = head
+			continue
+		}
+
+		if cur.Val >= pre.Val {
+			pre = cur
+			continue
+		}
+
+		tmpPre = res
+		for tmp := res.Next; tmp != pre.Next; tmp = tmp.Next {
+			if cur.Val <= tmp.Val {
+				pre.Next = cur.Next
+				tmpPre.Next = cur
+				cur.Next = tmp
+				cur = pre
+				break
+			}
+			tmpPre = tmp
+		}
+	}
+
+	return res.Next
 }
